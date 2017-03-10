9aa1dcb79900e177e695a1fb85786da13ee3e0f3
Richard Shade <rshade@rightscale.com>
2017-03-10 16:40:59 -0600

    updating to have base script

--- metadata.rb
+++ metadata.rb
@@ -17,3 +17,5 @@ version '0.1.0'
 # a Supermarket.
 #
 # source_url 'https://github.com/<insert_org_here>/rs-iis' if respond_to?(:source_url)
+depends 'iis'
+depends 'seven_zip'
--- recipes/default.rb
+++ recipes/default.rb
@@ -3,3 +3,27 @@
 # Recipe:: default
 #
 # Copyright:: 2017, The Authors, All Rights Reserved.
+#
+remote_file ::File.join(Chef::Config['file_cache_path'], 'BlogEngineNet33.zip') do
+  source 'https://s3.amazonaws.com/rs-professional-services-publishing/tmp/BlogEngineNet33.zip'
+  action :create
+end
+
+seven_zip_archive 'seven_zip_source' do
+  path      'C:\BlogEngineNet33'
+  source    ::File.join(Chef::Config['file_cache_path'], 'BlogEngineNet33.zip')
+  overwrite true
+  timeout   30
+end
+
+# stop and delete the default site
+iis_site 'Default Web Site' do
+  action [:stop, :delete]
+end
+
+iis_site 'BlogEngineNet33' do
+  protocol :http
+  port 80
+  path 'C:\BlogEngineNet33'
+  action [:add,:start]
+end
